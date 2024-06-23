f882c54261d2fca55fadffa99804565db9196909
Tetiano4kaS <stecko332@gmail.com>
2024-06-24 00:32:14 +0300

    Add movie icon

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -6,12 +6,22 @@ import styles from "./Header.module.css"
 import UserComponent from "../UserContainer/UserComponent";
 import cn from "classnames";
 import {useAppSelector} from "../../hooks/useHooks";
+import {PiFilmSlateBold} from "react-icons/pi";
+import {useNavigate} from "react-router-dom";
 
 const HeaderComponent = () => {
-    const {theme} = useAppSelector(state => state.theme)
+    const {theme} = useAppSelector(state => state.theme);
+    const navigate = useNavigate();
+    const handleClickIcon = () => {
+        navigate('/movies')
+    }
+
     return (
         <div className={cn(styles.header, {[styles.header_dark!]: theme})}>
-            <ThemeSwitcher/>
+            <div className={styles.leftContainer}>
+                <PiFilmSlateBold className={styles.icon} onClick={handleClickIcon}/>
+                <ThemeSwitcher/>
+            </div>
             <div className={styles.rightContainer}><SearchComponent/>
                 <UserComponent/></div>
         </div>
